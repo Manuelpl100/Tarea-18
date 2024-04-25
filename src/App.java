e2561998d7fa45d7cd54b65e3b038fb5ec880571
Manuelpl100 <mpp0044@alu.medac.es>
2024-04-25 16:33:54 +0200

    Commit develop3

--- src/App.java
+++ src/App.java
@@ -2,6 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Develop!");
         System.out.println("Hello, Develop2!");
+        System.out.println("Hello, Develop3!");
+
 
     }
 }
