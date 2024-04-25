393dd10dc1f0d473b743d4409eb5fb3fb45ce361
Manuelpl100 <mpp0044@alu.medac.es>
2024-04-25 16:41:22 +0200

    Commit fusion

--- src/App.java
+++ src/App.java
@@ -2,6 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Feature!");
         System.out.println("Hello, Feature2!");
+        System.out.println("Hello, Fusion!");
+
 
     }
 }
