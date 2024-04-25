e3b35eaa93ad529d3d7ce617def7cb9f73c12374
Manuelpl100 <mpp0044@alu.medac.es>
2024-04-25 16:37:20 +0200

    Commit fusion

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Hotflix!");
+        System.out.println("Hello, Feature!");
     }
 }
